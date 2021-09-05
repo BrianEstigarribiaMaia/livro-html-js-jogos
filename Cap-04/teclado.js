a720c9475130b13d2194c4e29ee155204a0c192d
Brian Estigarribia Maia <82018390+BrianEstigarribia@users.noreply.github.com>
2021-09-05 20:18:44 -0300

    Add files via upload

---
+++ Cap-04/teclado.js
@@ -0,0 +1,38 @@
+var SETA_ESQUERDA = 37;
+var SETA_DIREITA = 39;
+var ESPACO = 32;
+
+function Teclado(elemento) {
+    this.elemento = elemento;
+
+    this.pressionadas = [];
+    this.disparadas = [];
+    this.funcoesDisparo = [];
+
+    var teclado = this;
+
+    elemento.addEventListener('keydown', function(evento) {
+       var tecla = evento.keyCode;
+       teclado.pressionadas[tecla] = true; 
+
+       if (teclado.funcoesDisparo[tecla] && !teclado.disparadas[tecla]) {
+           teclado.disparadas[tecla] = true;
+           teclado.funcoesDisparo[tecla]();
+
+       }
+    });
+
+    elemento.addEventListener('keyup', function(evento){
+        teclado.pressionadas[evento.keyCode] = false;
+        teclado.disparadas[evento.keyCode] = false;
+    });
+}
+
+Teclado.prototype = {
+    pressionada: function (tecla) {
+         return this.pressionadas[tecla];
+    } ,
+     disparou: function (tecla, callback) {
+        this.funcoesDisparo[tecla] = callback;
+    }
+}
